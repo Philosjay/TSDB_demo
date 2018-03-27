431060a5496fcc17bf84996b6efdba07b816fd0f
philosjay <305546044@qq.com>
2018-03-27 20:55:45 +0800

    test_for_non_Batch_AND_static_Connection : 72.086s for 2000 inserts.

--- src/main/java/app/DBServer.java
+++ src/main/java/app/DBServer.java
@@ -137,12 +137,9 @@ public class  DBServer {
 
 
             // 每次录入信息及时生成一个InfoDao
-            InfoDao dao = new InfoDao();
-
-            dao.prepareConnection();
+            InfoDao dao = daoManager.getDao(tableName);
             HashMap<String,Object> info = new HashMap<String, Object>(req.getColumnInfoMap());
             dao.addInfo_withoutBatch(info, tableName);
-            dao.closeConnection();
 
 
             TableResponse reply = TableResponse.newBuilder().setMesg("record info for : " + tableName + " success").build();
